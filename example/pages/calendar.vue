6dff9a5beaaaf5affae356d74a9749ac6154e738
GilbertSun <szb4321@gmail.com>
2016-08-11 16:20:43 +0800

    add calendar demo

--- example/pages/calendar.vue
+++ example/pages/calendar.vue
@@ -2,11 +2,37 @@
   <div class="page-calendar">
     <div class="page-title">Calendar</div>
     <mt-calendar></mt-calendar>
+    <mt-calendar
+      :decorate="decorate"
+      :view="'month'"
+      :selected.sync="selected">
+    </mt-calendar>
+    <mt-calendar
+      :decorate="decorate"
+      :view="'week'"
+      :selected.sync="selected">
+    </mt-calendar>
+    <mt-calendar
+      :start-monday="true"
+      :decorate="decorate"
+      :view="'week'"
+      :selected.sync="selected">
+    </mt-calendar>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'page-calendar'
+  name: 'page-calendar',
+  data() {
+    return {
+      selected: new Date(),
+      decorate: {
+        '2016-08-09': true,
+        '2016-08-10': true,
+        '2016-08-11': true
+      }
+    };
+  }
 };
 </script>
